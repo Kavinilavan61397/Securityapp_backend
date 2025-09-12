cf7208beae1ec6fa16e4ea7d632b707c56b85deb
Kavinilavan K <kavinilavan613@gmail.com>
2025-09-12 14:22:43 +0530

    server fixes
    server fixes

--- api/server.js
+++ api/server.js
@@ -19,7 +19,6 @@ const photoRoutes = require('./src/routes/photo');
 const analyticsRoutes = require('./src/routes/analytics');
 const familyMemberRoutes = require('./src/routes/familyMember');
 const vehicleRoutes = require('./src/routes/vehicle');
-// const preApprovalRoutes = require('./src/routes/preApproval'); // Temporarily disabled
 
 // Initialize Express app
 const app = express();
@@ -118,7 +117,6 @@ app.use('/api/photos', photoRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/family-members', familyMemberRoutes);
 app.use('/api/vehicles', vehicleRoutes);
-// app.use('/api/pre-approvals', preApprovalRoutes); // Temporarily disabled
 
 // API base endpoint
 app.get('/api', (req, res) => {
@@ -139,7 +137,6 @@ app.get('/api', (req, res) => {
         analytics: `${BASE_URL}/api/analytics`,
         familyMembers: `${BASE_URL}/api/family-members`,
         vehicles: `${BASE_URL}/api/vehicles`,
-        // preApprovals: `${BASE_URL}/api/pre-approvals`, // Temporarily disabled
         documentation: `${BASE_URL}/api/docs`
       },
     environment: NODE_ENV
